a98f118b9b6036b9c5f8f656dba476fe3a3077f8
jason reaves <sysopfb@gmail.com>
2015-10-25 21:05:57 -0500

    Added a few more header files

---
+++ ch4/dbgmsg.h
@@ -0,0 +1,4 @@
+#ifdef LOG_OFF
+#define DBG_TRACE(src,msg)
+#define DBG_PRINT1(arg)
+#define 
---
+++ ch4/device.h
@@ -0,0 +1,4 @@
+const WCHAR DeviceNameBuffer[] = L"\\Device\\msnetdiag";
+const WCHAR DeviceLinkBuffer[] = L"\\DosDevices\\msnetdiag";
+const char UserlandPath[] = "\\\\.\\msnetdiag";
+
